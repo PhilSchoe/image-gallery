2eac7eb97a328b80dd635d97cf134910aeb15045
philippschoemacker <p.schoemacker@gmx.de>
2023-07-10 21:41:40 +0200

    Make images quadratic

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 const cards = [
-  { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
-  { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
-  { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 }
+  { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg' },
+  { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg' },
+  { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg' },
+  { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg' },
+  { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg' }
 ];
 </script>
 
@@ -24,12 +26,12 @@ const cards = [
       <v-container fluid class="w-75">
         <v-row>
           <v-col v-for="card in cards" :key="card.title">
-            <v-card>
+            <v-card style="width: 20em">
               <v-img
                 :src="card.src"
                 class="align-end"
                 gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                height="200px"
+                aspect-ratio="1"
                 cover
               >
                 <v-card-title class="text-white" v-text="card.title"></v-card-title>
