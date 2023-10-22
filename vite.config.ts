bdcfbf0adca138eef20de879f6bb6c9ba36270bf
PhilSchoe <p.schoemacker@gmx.de>
2023-10-22 17:38:31 +0200

    Add and configure vite-plugin-checker for vue-tsc

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+    // tsconfig.vitest.json extends tsconfig.app.json, therefore vitest is set to include both
+    checker({ vueTsc: { tsconfigPath: 'tsconfig.vitest.json' } })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
