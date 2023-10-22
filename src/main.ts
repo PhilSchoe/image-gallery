bce3c9793cf3a16b770016ed3cdb82871a194d24
PhilSchoe <p.schoemacker@gmx.de>
2023-10-22 18:17:03 +0200

    Setup mock service worker instance and start it

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import createVuetify from '@/plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 
+import { worker } from './mocks/browser';
+
+worker.start();
+
 const app = createApp(App);
 
 app.use(createVuetify);
