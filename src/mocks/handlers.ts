1b0c0013ba83eab1f2b4d80da4715e37c2409ed9
PhilSchoe <p.schoemacker@gmx.de>
2023-11-13 21:39:17 +0100

    Update mock service worker to version 2.0.5

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,46 +1,37 @@
-import { rest } from 'msw';
+import { HttpResponse, http } from 'msw';
 import { type Image } from '@/types/image';
 
 const isAuthenticatedKey = 'is-authenticated';
 
+const images: Array<Image> = [];
+
 export const handlers: any = [
-  rest.post('/login', (req, res, ctx) => {
+  http.post('/login', () => {
     // Persist user's authentication in the session
     sessionStorage.setItem(isAuthenticatedKey, 'true');
 
     // Respons with a 200 status code
-    return res(ctx.status(200));
+    return new HttpResponse(null, { status: 200 });
   }),
 
-  rest.get('/user', (req, res, ctx) => {
+  http.get('/user', (): HttpResponse => {
     // Check if the user is authenticated in this session
     const isAuthenticated = sessionStorage.getItem(isAuthenticatedKey);
 
     if (!isAuthenticated) {
       // If not authenticated, respond with a 403 error
-      return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: 'Not authorized'
-        })
-      );
+      return HttpResponse.json({ errorMessage: 'Not authorized' }, { status: 403 });
     }
 
     // If authenticated, return a mocked user details
-    return res(
-      ctx.status(200),
-      ctx.json({
-        username: 'admin'
-      })
-    );
+    return HttpResponse.json({ username: 'admin' }, { status: 200 });
   }),
 
-  rest.get('/images', (req, res, ctx) => {
-    const images: Array<Image> = [];
+  http.get('/image/all', () => {
     for (let i = 0; i < 6; i++) {
       images.push({ id: i, title: `Random_${i}`, src: `https://picsum.photos/30${i}` }); // Change size a little for different pictures
     }
 
-    return res(ctx.status(200), ctx.json(images));
+    return HttpResponse.json(images, { status: 200 });
   })
 ];
